8c89d0076fc4154e278bd8578ed05764252414d7
KASALA AKASH DEEP GOUD <88823440+kasalaakashdeepgoud@users.noreply.github.com>
2022-07-04 22:04:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-//const CONNECTION_URL = 'mongodb+srv://kasalaakashdeepgoud:kasalaakashdeepgoud123@cluster0.jeqyf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://userid:password@cluster0.jeqyf.mongodb.net/Databasename?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 5000;
 
